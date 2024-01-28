2d61ed160b2d4240fb8fe04da35f888968d55dae
Hugo <hugosantosadm125@gmail.com>
2024-01-28 18:27:30 -0300

    Introdução a vetores em C #

---
+++ IntroducaoVetores/IntroducaoVetores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace IntroducaoVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração das Variaveis:
+            int N;
+            double[] vet;
+
+            //Solitação de valor ao usuário: Quantos números serão inseridos no vetor
+            N = int.Parse(Console.ReadLine());
+
+            //Instanciação do Vetor com quantidade
+            vet = new double[N];
+
+            //Laço FOR: Para inserir valores dentro do vetor
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            //Laço FOR: Para conferir o valores digitados dentro do vetor
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(vet[i].ToString("F1",CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
